cff04c00bb4bf10ae7b6d3aa415e10c19822a63c
reihsr <robert.reihs@gmail.com>
2016-10-24 19:43:06 +0200

    Update installation script

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ usage()
     echo ""
     echo "Example:"
     echo "       sudo ./start.sh -i instance1"
+    echo ""
 }
 
 version()
@@ -48,21 +49,19 @@ updateConfigurationFile()
     
     sed -i "s/§§INSTANCE/${instance}/g" "$folder/docker-compose.yml"
     sed -i "s/§§FOLDER/${folder}/g" "$folder/docker-compose.yml"
-
-
-§§MYSQL_ROOT_PASSWORD
-§§MYSQL_DATABASE
-§§MYSQL_USER
-§§MYSQL_PASSWORD
-§§TOMCAT_MANAGER_USER
-§§TOMCAT_MANAGER_PASSWORD
-§§INSTITUTE_NAME
-§§EMAIL_ADDRESS
-§§FIRST_NAME
-§§LAST_NAME
-§§LOGIN_NAME
-§§ADDRESS
-      
+    sed -i "s/§§PORT/${port}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§MYSQL_ROOT_PASSWORD/${MYSQL_ROOT_PASSWORD}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§MYSQL_DATABASE/${MYSQL_DATABASE}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§MYSQL_USER/${MYSQL_USER}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§TOMCAT_MANAGER_USER/${TOMCAT_MANAGER_USER}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§TOMCAT_MANAGER_PASSWORD/${TOMCAT_MANAGER_PASSWORD}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§INSTITUTE_NAME/${INSTITUTE_NAME}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§EMAIL_ADDRESS/${EMAIL_ADDRESS}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§FIRST_NAME/${FIRST_NAME}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§LAST_NAME/${LAST_NAME}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§LOGIN_NAME/${LOGIN_NAME}/g" "$folder/docker-compose.yml"
+    sed -i "s/§§ADDRESS/${ADDRESS}/g" "$folder/docker-compose.yml"
 }
 
 createFolders()
@@ -85,10 +84,87 @@ checkParameters()
     else
         echo "Instance: $instance" 
     fi
+    
     if [[ "$folder" = "/opt/bibbox/application-instance/app-openspecimen-instance" ]]; then
         echo "No installation folder set! Using default."
     else
         echo "Installation Folder: $folder" 
+    fi
+    
+    if [[ -z "$port" ]]; then
+        usage
+        error_exit "The port is not set."
+    else
+        echo "Port: $port"
+    fi
+    
+    if [[ -z "$MYSQL_ROOT_PASSWORD" ]]; then
+        echo "Mysql root passwort not set, creating random password."
+        MYSQL_ROOT_PASSWORD=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= < /dev/urandom | head -c 32 | xargs)
+    else
+        echo "Mysql root passwort: ****"
+    fi
+    
+    if [[ -z "$MYSQL_DATABASE" ]]; then
+        echo "Database not set"
+    else
+        echo "MYSQL_DATABASE: $MYSQL_DATABASE"
+    fi
+    
+    if [[ -z "$MYSQL_USER" ]]; then
+        echo "Mysql User not set"
+    else
+        echo "MYSQL_USER: $MYSQL_USER"
+    fi
+    
+    if [[ -z "$MYSQL_PASSWORD" ]]; then
+        echo "Mysql password not set, creating random password."
+        MYSQL_PASSWORD=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= < /dev/urandom | head -c 32 | xargs)
+    else
+        echo "MYSQL_PASSWORD: ****"
+    fi
+    
+    if [[ -z "$TOMCAT_MANAGER_USER" ]]; then
+        echo "Tomcat manager user not set"
+    else
+        echo "TOMCAT_MANAGER_USER: $TOMCAT_MANAGER_USER"
+    fi
+    
+    if [[ -z "$TOMCAT_MANAGER_PASSWORD" ]]; then
+        echo "Tomcat manager password not set, creating random password."
+        TOMCAT_MANAGER_PASSWORD=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= < /dev/urandom | head -c 32 | xargs)
+    else
+        echo "TOMCAT_MANAGER_PASSWORD: ****"
+    fi  
+    
+    if [[ -z "$EMAIL_ADDRESS" ]]; then
+        echo "email address for openspecimen user not set"
+    else
+        echo "EMAIL_ADDRESS: $EMAIL_ADDRESS"
+    fi
+    
+    if [[ -z "$FIRST_NAME" ]]; then
+        echo "First name for openspecimen user not set"
+    else
+        echo "FIRST_NAME: $FIRST_NAME"
+    fi
+    
+    if [[ -z "$LAST_NAME" ]]; then
+        echo "Last name for openspecimen user not set"
+    else
+        echo "LAST_NAME: $LAST_NAME"
+    fi
+    
+    if [[ -z "$LOGIN_NAME" ]]; then
+        echo "Login user name for openspecimen user not set"
+    else
+        echo "LOGIN_NAME: $LOGIN_NAME"
+    fi
+    
+    if [[ -z "$ADDRESS" ]]; then
+        echo "address for openspecimen user not set"
+    else
+        echo "ADDRESS: $ADDRESS"
     fi
 }
 
@@ -99,6 +175,45 @@ while [ "$1" != "" ]; do
                                 ;;
         -f | --folder )         shift
                                 folder=$1
+                                ;;
+        -p | --port )           shift
+                                port=$1
+                                ;;
+        --mysqlrootpw )         shift
+                                MYSQL_ROOT_PASSWORD=$1
+                                ;;
+        --mysqldatabase )       shift
+                                MYSQL_DATABASE=$1
+                                ;;
+        --mysqluser )           shift
+                                MYSQL_USER=$1
+                                ;;
+        --mysqlpassword )       shift
+                                MYSQL_PASSWORD=$1
+                                ;;
+        --tomcatmanager )       shift
+                                TOMCAT_MANAGER_USER=$1
+                                ;;
+        --tomcatpassword )      shift
+                                TOMCAT_MANAGER_PASSWORD=$1
+                                ;;
+        --institutename )       shift
+                                INSTITUTE_NAME=$1
+                                ;;
+        --email )               shift
+                                EMAIL_ADDRESS=$1
+                                ;;
+        --firstname )           shift
+                                FIRST_NAME=$1
+                                ;;
+        --lastname )            shift
+                                LAST_NAME=$1
+                                ;;
+        --login )               shift
+                                LOGIN_NAME=$1
+                                ;;
+        --address )             shift
+                                ADDRESS=$1
                                 ;;
         -h | --help )           usage
                                 clean_up

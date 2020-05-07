18818fc325d523ff200e2321d442cf67fbb2ce88
ChristianMUG <christian.schorn@medunigraz.at>
2020-05-07 12:51:38 +0200

    update to build from war file

--- images/openspecimen/scripts/entrypoint.sh
+++ images/openspecimen/scripts/entrypoint.sh
@@ -7,6 +7,6 @@ echo "Wait for DB server to be ready"
 /opt/scripts/waitforit.sh "${DATABASE_HOST}:${DATABASE_PORT}"
 
 
-/var/lib/tomcat9/bin/catalina.sh start
+/usr/share/tomcat9/bin/catalina.sh start
 
 tail -f /var/lib/tomcat9/logs/catalina.out -f /var/lib/openspecimen/data/logs/os.log

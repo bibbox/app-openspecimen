53d4deec8fc2a0c0c94ceda3d1dc1a3f80bba4eb
Simon Streit <streit.simon5@gmail.com>
2021-12-02 17:38:15 +0100

    update data folder with stuff

--- configs/openspecimen/scripts/entrypoint.sh
+++ configs/openspecimen/scripts/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+python3 -m pip install /var/lib/openspecimen/plugins/OpenSpecimenAPIconnector-0.9.2-py3-none-any.whl
 echo "Starting OpenSpecimen Application!"
 
 . defaultvar.sh
 
 echo "Wait for DB server to be ready"
 /opt/scripts/waitforit.sh "${DATABASE_HOST}:${DATABASE_PORT}"
-python3 -m pip install /var/lib/openspecimen/plugins/OpenSpecimenAPIconnector-0.9.2-py3-none-any.whl
+
 
 /usr/share/tomcat9/bin/catalina.sh start
 

b6a839208563430b21608442f03041cc6dbddfac
Simon Streit <streit.simon5@gmail.com>
2022-05-24 14:37:31 +0200

    update 9.0 used internal tomcat user to starup Openspecimen

--- configs/openspecimen/scripts/entrypoint.sh
+++ configs/openspecimen/scripts/entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
-python3 -m pip install /var/lib/openspecimen/plugins/OpenSpecimenAPIconnector-0.9.2-py3-none-any.whl
 echo "Starting OpenSpecimen Application!"
 
 . defaultvar.sh
 
 sed -e "s/__DB_PW/$MYSQL_PASSWORD/g" -e "s/__DB_USER/$MYSQL_USER/g" /var/lib/tomcat9/conf/context.xml.template > /var/lib/tomcat9/conf/context.xml
 
+#chown -R tomcat:tomcat /var/lib/openspecimen/plugins
+#chown -R tomcat:tomcat /var/lib/openspecimen/data
+#chown -R tomcat:tomcat /var/lib/tomcat9/webapps/ROOT
+
 echo "Wait for DB server to be ready"
 /opt/scripts/waitforit.sh "${DATABASE_HOST}:${DATABASE_PORT}"
 
-
 /usr/share/tomcat9/bin/catalina.sh start
 
-tail -f /var/lib/tomcat9/logs/catalina.out -f /var/lib/openspecimen/data/logs/os.log
+tail -f /var/lib/tomcat9/logs/catalina.out -f /var/lib/tomcat9/openspecimen/data/logs/os.log
